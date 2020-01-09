99fb6cb941dc550142f3e13e2c28717457f62fbc
Victor Rigoberto <jsvicctor@gmail.com>
2020-01-08 21:10:26 -0600

    Instalacion Bootstrap Jquery Popper

--- FrontApp/src/app/app.module.ts
+++ FrontApp/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
